684db21994518b1fab8595fc9f54b8898c28993a
andres <andy_l3a@hotmail.com>
2025-04-26 12:56:44 -0500

    Merge pull request #2 from AndresUPS93/feature1
    FIREBASE AUTH ANDROID

---
+++ Crypto-Android-2025-main/app/src/main/java/com/example/coincapapp/authentication/AuthModule.kt
@@ -0,0 +1,17 @@
+package com.example.coincapapp.authentication
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class AuthModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthClient(
+        firebaseAuthClient: FirebaseAuthClient
+    ): AuthClient
+}
